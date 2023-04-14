bb7fcdf6ef895a73527527f16d4ad9bec141570c
VeronikaVS90 <117116182+VeronikaVS90@users.noreply.github.com>
2023-04-14 20:01:58 +0000

    Deploying to gh-pages from @ VeronikaVS90/goit-js-hw-09@9068b990e7d087e6c9a8046e095908626c228fa0 🚀

---
+++ 01-color-switcher.89998488.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");console.dir(t),t.disabled=!0;let a=null;e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.89998488.js.map
