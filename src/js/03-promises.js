a9c3f225e0220b0076824e329358f0fb2315373c
VeronikaVS90 <vsokolovanika@gmail.com>
2023-04-16 23:56:26 +0300

    Виконала 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+let mainDelay = 0;
+
+form.addEventListener('submit', promiceGenerator);
+
+function promiceGenerator(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  mainDelay = delayValue;
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, mainDelay);
+    mainDelay += stepValue;
   }
+}
+
+
+function createPromise(position, delay) {
+  const promice = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promice
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.success(`❌ Rejected promise ${position} in ${delay}ms`, {
+        success: {
+          background: '#ff5549',
+        },
+      });
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
